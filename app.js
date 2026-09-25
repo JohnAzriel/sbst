6394666d01a8efe5c09784910f4bdbe77c862067
JohnAzriel <johnazrielperez@gmail.com>
2026-09-25 19:51:21 +0800

    3rd commit

--- app.js
+++ app.js
@@ -5,29 +5,24 @@ const app = express();
 const path = require("path");
 const session = require("express-session");
 const flash = require("express-flash");
-const cors = require('cors');
-const port = 8888;
-
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended:true}));
-app.use(session({
-    secret: 'abc',
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "abc",
     resave: false,
     saveUninitialized: true,
-}));
+  }),
+);
 app.use(cors());
 app.use(flash());
 app.set("view-engine", "ejs");
-app.listen(port, (err)=>{
-    if(err){
-        console.log(err.message);
-        return;
-    }
-    else{
-        console.log("Running at " + port);
-    }
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 app.use("/", require("./routes/mainRoute"));
